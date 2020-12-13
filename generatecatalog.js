6f5114a60fd54b2dea39084ded6e48e7fd49a3f4
broken-admin <brokengaming50@gmail.com>
2020-12-12 23:33:28 -0600

    Multiple changes + Update generatecatalog.js + Add custom stylesheet

--- generatecatalog.js
+++ generatecatalog.js
@@ -17,7 +17,8 @@ fs.writeSync(outputFile, "<head>");
 fs.writeSync(outputFile, "<title>techlog</title>\n");
 // Viewport to make site responsive
 fs.writeSync(outputFile, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
-fs.writeSync(outputFile, "<link rel=\"stylesheet\" href=\"/techlog/assets/css/style.css\">\n")
+fs.writeSync(outputFile, "<link rel=\"stylesheet\" href=\"/techlog/assets/css/style.css\">\n");
+fs.writeSync(outputFile, "<link rel=\"stylesheet\" href=\"/techlog/style/custom.css\">\n");
 fs.writeSync(outputFile, "</head>\n<body>");
 
 // Write the main page header
