c85ecf8adb78ef18645166332b62e095657829d4
broken-admin <brokengaming50@gmail.com>
2020-12-13 00:50:47 -0600

    update generatecatalog.js

--- generatecatalog.js
+++ generatecatalog.js
@@ -14,7 +14,7 @@ let outputFile = fs.openSync("catalog.html", "w");
 
 // Set up the main file
 fs.writeSync(outputFile, "<head>");
-fs.writeSync(outputFile, "<title>techlog</title>\n");
+fs.writeSync(outputFile, "<title>techlog: catalog</title>\n");
 // Viewport to make site responsive
 fs.writeSync(outputFile, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
 fs.writeSync(outputFile, "<link rel=\"stylesheet\" href=\"/techlog/assets/css/style.css\">\n");
